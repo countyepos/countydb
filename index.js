bbe36daf056c31ac8c2d076e4fd3c051d5f1667a
countyepos <grahame@countyepos.com>
2025-07-26 11:54:35 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+echo "# countydb" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/countyepos/countydb.git
+git push -u origin main
